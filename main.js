423a338cc0933fee17fef2bf1826fe5e524b0ca3
Prananda Yoga <pranandayoga21@gmail.com>
2024-02-01 11:41:52 +0800

    add main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(morgan("short"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Bingleshop!");
+});
+
+// add router below this comment
+
+app.listen(port, () => {
+  console.log(`This app running at http://localhost:${port}`);
+});
