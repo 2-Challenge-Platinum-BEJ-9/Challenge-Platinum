e810036e6826fbe74d1877c823f17092ef1fd6d3
= <sabannurrahman@gmail.com>
2024-02-03 14:24:59 +0800

    migration and modal user

--- models/user.js
+++ models/user.js
@@ -14,15 +14,103 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    address: DataTypes.STRING,
-    password: DataTypes.TEXT,
-    token: DataTypes.TEXT,
-    role: DataTypes.STRING,
-    image: DataTypes.TEXT
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "first name is empty"
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "last name is empty"
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "email is already"
+      },
+      isEmail: true,
+      validate: {
+        notNull: {
+          msg: "Email is empty"
+        },
+        isEmail: {
+          msg: "Incorrect email format"
+        },
+      },
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "phone number is already"
+      },
+      validate: {
+        notNull: {
+          msg: "Email is empty"
+        },
+      },
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "address is empty"
+        },
+      },
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Password is empty"
+        },
+        len: {
+          args: [6, 100],
+          msg: "Password must be at least 6-100 characters"
+        },
+      },
+    },
+    token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "token is empty"
+        },
+      },
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "role is empty"
+        },
+      },
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "image is empty"
+        },
+      },
+    },
   }, {
     sequelize,
     modelName: 'User',
