3a89bc98b5e05bb94b2e7af5c19926c6aec7bcdb
= <sabannurrahman@gmail.com>
2024-02-04 20:37:49 +0800

    update miggration and modal user

--- migrations/20240202015059-create-user.js
+++ migrations/20240202015059-create-user.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      userId: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
