30c2de1083e6343e3d2bfc39ee8442da015b1cac
Prananda Yoga <pranandayoga21@gmail.com>
2024-02-04 12:40:46 +0800

    menambahkan inline commnent terkait detail endpoint

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const { methodNotAllowed } = require("../helper/errorHandler");
 
-router.route("/auth").post().all(methodNotAllowed);
-router.route("/login").post().delete().all(methodNotAllowed);
+router.route("/auth").post().all(methodNotAllowed); // endpoint http://localhost:3000/api/v1/auth
+router.route("/login").post().delete().all(methodNotAllowed); // endpoint http://localhost:3000/api/v1/login
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const { methodNotAllowed } = require("../helper/errorHandler");
 
-router.route("/orders").get().post().all(methodNotAllowed);
-router.route("/orders/:id").get().put().all(methodNotAllowed);
+router.route("/orders").get().post().all(methodNotAllowed); // endpoint http://localhost:3000/api/v1/orders
+router.route("/orders/:id").get().put().all(methodNotAllowed); // endpoint http://localhost:3000/api/v1/orders/:id
