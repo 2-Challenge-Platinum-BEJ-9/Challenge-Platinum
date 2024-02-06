bdc8fa343d68e108594bcc8bed703b80c180a094
Prananda Yoga <pranandayoga21@gmail.com>
2024-02-06 18:28:35 +0800

    menambahkan comment inline pada authRuter.js

--- router/authRouter.js
+++ router/authRouter.js
@@ -2,4 +2,4 @@ const router = require("express").Router();
 const { methodNotAllowed } = require("../helper/errorHandler");
 
 router.route("/").post().all(methodNotAllowed); // endpoint /api/v1/auth
-router.route("/login").post().delete().all(methodNotAllowed); // endpoint /api/v1/login
+router.route("/login").post().delete().all(methodNotAllowed); // endpoint /api/v1/auth/login
