d3a4ea46784787b5972db11cd015834015507ced
Prananda Yoga <pranandayoga21@gmail.com>
2024-02-07 18:03:48 +0800

    perbaikan router

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,3 +3,5 @@ const { methodNotAllowed } = require("../helper/errorHandler");
 
 router.route("/").post().all(methodNotAllowed); // endpoint /api/v1/auth
 router.route("/login").post().delete().all(methodNotAllowed); // endpoint /api/v1/auth/login
+
+module.exports = router;
--- router/itemRouter.js
+++ router/itemRouter.js
@@ -3,3 +3,5 @@ const { methodNotAllowed } = require("../helper/errorHandler");
 
 router.route("/").get().post().all(methodNotAllowed); // endpoint /api/v1/items
 router.route("/:id").get().put().delete().all(methodNotAllowed); // endpoint /api/v1/items/:id
+
+module.exports = router;
