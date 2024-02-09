a923367e3e2e573d1ef28e566868965c7a330bc1
= <sabannurrahman@gmail.com>
2024-02-09 11:06:27 +0800

    crete user controller and config endpoint user

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const { methodNotAllowed } = require("../helper/errorHandler");
+const { userController } = require("../Controller/userController");
 
-router.route("/").get().all(methodNotAllowed); // endpoint /api/v1/users
+router.route("/").get(userController).all(methodNotAllowed); // endpoint /api/v1/users
 router.route("/:id").get().delete().all(methodNotAllowed); // endpoint /api/v1/users/:id
+
+module.exports = router;
