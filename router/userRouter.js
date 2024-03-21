259955783db6c00b571d9879ac6e86fab5c4b079
= <sabannurrahman@gmail.com>
2024-03-21 16:43:47 +0800

    perbaikan controller

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { methodNotAllowed } = require("../helper/errorHandler");
-const { userController } = require("../Controller/userController");
+const { UserController } = require("../Controller/userController");
 
-router.route("/").get(userController).all(methodNotAllowed); // endpoint /api/v1/users
-router.route("/:id").get().delete().all(methodNotAllowed); // endpoint /api/v1/users/:id
+router.route("/").get(UserController.allUsers).all(methodNotAllowed); // endpoint /api/v1/users
+router.route("/deleteUser:id").delete(UserController.deleteUser).all(methodNotAllowed); // endpoint /api/v1/users/:id
+router.route("/detailUser:id").get(UserController.detailUser).all(methodNotAllowed); // endpoint /api/v1/users/:id
 
 module.exports = router;
