7c900040bc78cc84b8443cc4bf1dd631a4565fd4
Prananda Yogathama Pribadi <pranandayoga21@gmail.com>
2024-03-25 00:21:12 +0800

    enhance uploadController and logger export

--- helper/logger.js
+++ helper/logger.js
@@ -2,37 +2,37 @@ const winston = require("winston");
 const DailyRotateFile = require("winston-daily-rotate-file");
 
 const logger = winston.createLogger({
-  level: "error",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.ms(),
-    winston.format.prettyPrint()
-  ),
-  transports: [
-    new DailyRotateFile({
-      level: "error",
-      filename: "./log/app-error-%DATE%.log",
-      zippedArchive: true,
-      maxSize: "2m",
-      maxFiles: "14d",
-      handleExceptions: true,
-      handleRejections: true,
-    }),
-    new DailyRotateFile({
-      level: "warn",
-      filename: "./log/app-exception-%DATE%.log",
-      zippedArchive: true,
-      maxSize: "2m",
-      maxFiles: "14d",
-    }),
-    new DailyRotateFile({
-      level: "info",
-      filename: "./log/app-info-%DATE%.log",
-      zippedArchive: true,
-      maxSize: "2m",
-      maxFiles: "14d",
-    }),
-  ],
+	level: "error",
+	format: winston.format.combine(
+		winston.format.timestamp(),
+		winston.format.ms(),
+		winston.format.prettyPrint()
+	),
+	transports: [
+		new DailyRotateFile({
+			level: "error",
+			filename: "./log/app-error-%DATE%.log",
+			zippedArchive: true,
+			maxSize: "2m",
+			maxFiles: "14d",
+			handleExceptions: true,
+			handleRejections: true,
+		}),
+		new DailyRotateFile({
+			level: "warn",
+			filename: "./log/app-exception-%DATE%.log",
+			zippedArchive: true,
+			maxSize: "2m",
+			maxFiles: "14d",
+		}),
+		new DailyRotateFile({
+			level: "info",
+			filename: "./log/app-info-%DATE%.log",
+			zippedArchive: true,
+			maxSize: "2m",
+			maxFiles: "14d",
+		}),
+	],
 });
 
-module.exports = { logger };
+module.exports = logger;
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 
 const router = require("./router/index");
 const passport = require("./middleware/passport");
-const { logger } = require("./helper/logger");
+const logger = require("./helper/logger");
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,42 +17,42 @@ app.use(express.json());
 app.use(passport.initialize());
 
 app.get("/", (req, res) => {
-  let message = "Welcome to Bingleshop!";
-  res.json({ message });
+	let message = "Welcome to Bingleshop!";
+	res.json({ message });
 });
 
 app.use("/api/v1", router);
 
 app.use((err, req, res, next) => {
-  logger.error(err.message);
-  return res.status(500).json({ message: err.message });
+	logger.error(err.message);
+	return res.status(500).json({ message: err.message });
 });
 
 let adminId = "";
 
 // Socket.IO logic
 io.on("connection", (socket) => {
-  console.log("A user connected");
+	console.log("A user connected");
 
-  if (socket.handshake.query.is_admin) {
-    adminId = socket.id;
-  }
+	if (socket.handshake.query.is_admin) {
+		adminId = socket.id;
+	}
 
-  socket.on("send chat", (data) => {
-    const { user_id, message } = data;
-    if (socket.handshake.query.is_admin) {
-      io.to(user_id).emit("receive chat", { message });
-    } else {
-      io.to(adminId).emit("receive chat", {
-        user_id: socket.id,
-        message: message,
-      });
-    }
-  });
+	socket.on("send chat", (data) => {
+		const { user_id, message } = data;
+		if (socket.handshake.query.is_admin) {
+			io.to(user_id).emit("receive chat", { message });
+		} else {
+			io.to(adminId).emit("receive chat", {
+				user_id: socket.id,
+				message: message,
+			});
+		}
+	});
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+	});
 });
 
 module.exports = httpServer;
