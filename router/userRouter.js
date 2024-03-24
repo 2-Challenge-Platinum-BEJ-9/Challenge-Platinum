959e612d73000b00ea0d05edb1cdb17710ace15f
Prananda Yogathama Pribadi <pranandayoga21@gmail.com>
2024-03-25 01:10:41 +0800

    initial init

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const { methodNotAllowed } = require("../middleware/methodProhibited");
+const { UserController } = require("../controller/userController");
 
-router.route("/").get().all(methodNotAllowed); // endpoint /api/v1/users
-router.route("/:id").get().delete().all(methodNotAllowed); // endpoint /api/v1/users/:id
+router.route("/").get(UserController.allUsers).all(methodNotAllowed); // endpoint /api/v1/users
+router
+	.route("/deleteUser:id")
+	.delete(UserController.deleteUser)
+	.all(methodNotAllowed); // endpoint /api/v1/users/:id
+router
+	.route("/detailUser:id")
+	.get(UserController.detailUser)
+	.all(methodNotAllowed); // endpoint /api/v1/users/:id
 
 module.exports = router;
