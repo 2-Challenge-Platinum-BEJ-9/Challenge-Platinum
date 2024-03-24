90882f1f863463cc05a89cfafa2af00bdad2be0b
Ardiansyah <4rdi17@gmail.com>
2024-03-24 18:00:27 +0700

    feat: socket.io

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const morgan = require("morgan");
+
 const router = require("./router/index");
-const app = express();
 const passport = require("./middleware/passport");
 const { logger } = require("./helper/logger");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 app.use(morgan("short"));
 app.use(express.json());
@@ -22,4 +28,31 @@ app.use((err, req, res, next) => {
   return res.status(500).json({ message: err.message });
 });
 
-module.exports = app;
+let adminId = "";
+
+// Socket.IO logic
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  if (socket.handshake.query.is_admin) {
+    adminId = socket.id;
+  }
+
+  socket.on("send chat", (data) => {
+    const { user_id, message } = data;
+    if (socket.handshake.query.is_admin) {
+      io.to(user_id).emit("receive chat", { message });
+    } else {
+      io.to(adminId).emit("receive chat", {
+        user_id: socket.id,
+        message: message,
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+module.exports = httpServer;
